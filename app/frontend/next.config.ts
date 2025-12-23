33cf4f920b2674e3bb92181c4d7de6b7a362cdbe
miho <paglinawan0520@gmail.com>
2025-12-23 15:45:58 +0900

    update: デプロイのための調整

--- app/frontend/next.config.ts
+++ app/frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/reread",
+  assetPrefix: "/reread/",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
